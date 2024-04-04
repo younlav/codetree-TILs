3c47a7694ebb392359d918ae238be8f00ed6849c
younlav <165302429+younlav@users.noreply.github.com>
2024-04-04 21:46:40 +0900

    Title: 문자열 순서 바꾸기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240404/문자열 순서 바꾸기/change-order-of-strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s[5];
+    char t[5];
+
+    scanf("%s\n%s", &s, &t);
+
+    printf("%s\n", t);
+    printf("%s\n", s);
+
+    
+    return 0;
+}
