e4c336f5b59575e7df5fbf3a265515dc66b4f582
younlav <165302429+younlav@users.noreply.github.com>
2024-04-04 21:23:32 +0900

    Title: 데이터 교환, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/데이터 교환/exchange-data.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=5, b=6, c=7;
+    int point;
+    point = a;
+    a = c;
+    c = b; 
+    b = point;
+
+    printf("%d\n%d\n%d", a, b, c);
+    return 0;
+}
