f4119f98d3fffc91f17f778f2f65fe7e0acfc8ed
younlav <165302429+younlav@users.noreply.github.com>
2024-04-04 21:26:27 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/합을 복사하기/copy-the-sum.c
+++ 240404/합을 복사하기/copy-the-sum.c
@@ -3,10 +3,9 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a = 1, b = 2, c = 3;
-    int sum = a + b + c;
-    a = sum;
-    b = sum;
-    c = sum;
+    a = b = c = a + b + c;
+
+
     printf("%d %d %d", a, b, c);
     return 0;
 }
